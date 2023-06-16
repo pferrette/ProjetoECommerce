553b202f9339326930eb65a7d0756873f9439851
guilherme-massao <guilherme.yoshicava@gmail.com>
2023-06-16 00:14:33 -0300

    Adição dos arquivos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce.Models;
+
+namespace ProjetoEcommerce.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+     public IActionResult ContaAdmin()
+    {
+        return View();
+    }
+
+    public IActionResult ProdutosConsultar()
+    {
+        return View();
+    }
+
+    public IActionResult ProdutosCadastrar()
+    {
+        return View();
+    }
+
+    public IActionResult Pedidos()
+    {
+        return View();
+    }
+
+    public IActionResult AdministradoresConsultar()
+    {
+        return View();
+    }
+
+    public IActionResult ClientesConsultar()
+    {
+        return View();
+    }
+
+public IActionResult RelatorioDiario()
+    {
+        return View();
+    }
+
+    public IActionResult RelatorioMensal()
+    {
+        return View();
+    }
+
+   public IActionResult RelatorioAnual()
+    {
+        return View();
+    }
+
+ public IActionResult RelatorioGlobal()
+    {
+        return View();
+    }
+
+     public IActionResult Configuracoes()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function esconderflexInnerListProdutos(){
+    let elemento = document.getElementById("flexInnerListProdutos");
+    if (elemento.style.display == "none"){
+        elemento.style.display = "flex";
+        let elemento2 = document.getElementById("botaoPaiProdutos");
+        elemento2.textContent= "Produtos ᐁ";
+    }else{
+        elemento.style.display = "none";
+        let elemento2 = document.getElementById("botaoPaiProdutos");
+        elemento2.textContent= "Produtos ᐅ";
+    }
+}
+
+function esconderflexInnerListUsuarios(){
+    let elemento = document.getElementById("flexInnerListUsuarios");
+    if (elemento.style.display == "none"){
+        elemento.style.display = "flex";
+        let elemento2 = document.getElementById("botaoPaiUsuarios");
+        elemento2.textContent= "Usuários ᐁ";
+    }else{
+        elemento.style.display = "none";
+        let elemento2 = document.getElementById("botaoPaiUsuarios");
+        elemento2.textContent= "Usuários ᐅ";
+    }
+}
+
+function esconderflexInnerListRelatorios(){
+    let elemento = document.getElementById("flexInnerListRelatorios");
+    if (elemento.style.display == "none"){
+        elemento.style.display = "flex";
+        let elemento2 = document.getElementById("botaoPaiRelatorios");
+        elemento2.textContent= "Relatórios ᐁ";
+    }else{
+        elemento.style.display = "none";
+        let elemento2 = document.getElementById("botaoPaiRelatorios");
+        elemento2.textContent = "Relatórios ᐅ";
+    }
+}
